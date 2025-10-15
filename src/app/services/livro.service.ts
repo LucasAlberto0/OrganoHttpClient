e3daca7069f30193d43363cc85b61278b4a7bbb7
Lucas Alberto <lucasalberto4321@gmail.com>
2025-10-15 13:25:04 -0300

    setup-inicial

---
+++ src/app/services/livro.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { GeneroLiterario, Livro } from '../componentes/livro/livro';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+
+    generos: GeneroLiterario[] = [
+    { id: 'romance', value: 'Romance' },
+    { id: 'misterio', value: 'Mistério' },
+    { id: 'fantasia', value: 'Fantasia' },
+    { id: 'ficcao-cientifica', value: 'Ficção Científica' },
+    { id: 'tecnicos', value: 'Técnicos' }
+  ];
+
+
+  private API_URL = "http://localhost:3000/livros"; 
+
+  constructor(private httpClient: HttpClient) { }
+
+  obterLivros(): Observable<Livro[]> {
+    return this.httpClient.get<Livro[]>(this.API_URL);
+  }
+
+    organizarLivrosPorGenero(): Observable<Map<string, Livro[]>> {
+    return this.obterLivros().pipe(
+      map((livros: Livro[]) => {
+        const livrosPorGenero = new Map<string, Livro[]>();
+
+        livros.forEach((livro: Livro) => {
+          const generoId =  typeof livro.genero === 'string' ? livro.genero : livro.genero?.id;
+
+          if(generoId) {
+            if(!livrosPorGenero.has(generoId)){
+              livrosPorGenero.set(generoId, [])
+            }
+            livrosPorGenero.get(generoId)?.push(livro)
+          }
+        })
+        return livrosPorGenero
+      })
+    )
+  }
+
+  adicionarLivro(novoLivro: Livro): Observable<Livro> {
+    return this.httpClient.post<Livro>(this.API_URL, novoLivro)
+  }
+
+}
